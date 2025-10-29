407476992c4e840e364c8c1574d935a47442e579
HimaVarshitha7 <231FA04A55@gmail.com>
2025-10-29 22:11:25 +0530

    commit

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,9 +1,8 @@
-// src/api/auth.js
 import axios from 'axios';
 
-const API_URL = "http://localhost:5000/api/auth";
+const API_URL = import.meta.env.VITE_API_URL || "http://localhost:5000/api/auth";
 
 export const registerUser = async (formData) => {
   const res = await axios.post(`${API_URL}/register`, formData);
-  return res.data; // returns { token, user }
+  return res.data;
 };
