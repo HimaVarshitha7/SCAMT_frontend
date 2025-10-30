05de972759c727851cff49f5d94cbb09842050f0
HimaVarshitha7 <231FA04A55@gmail.com>
2025-10-30 08:57:28 +0530

    changes

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = import.meta.env.VITE_API_URL || "http://localhost:5000/api/auth";
+const API_URL = import.meta.env.VITE_API_URL || "https://scamt.vercel.app/";
 
 export const registerUser = async (formData) => {
   const res = await axios.post(`${API_URL}/register`, formData);
