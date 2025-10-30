ba79e302d934c31a32cfb7fdb1ddf67c067b092a
HimaVarshitha7 <231FA04A55@gmail.com>
2025-10-30 10:57:24 +0530

    commit

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,8 +1,13 @@
 import axios from 'axios';
 
-const API_URL = import.meta.env.VITE_API_URL || "https://scamt-backend-1.onrender.com/";
+const API_URL = import.meta.env.VITE_API_URL || "https://scamt-backend-1.onrender.com/api";
 
 export const registerUser = async (formData) => {
-  const res = await axios.post(`${API_URL}/register`, formData);
+  const res = await axios.post(`${API_URL}/auth/register`, formData);
+  return res.data;
+};
+
+export const loginUser = async (formData) => {
+  const res = await axios.post(`${API_URL}/auth/login`, formData);
   return res.data;
 };
